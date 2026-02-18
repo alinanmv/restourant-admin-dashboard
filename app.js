e1c6fc97a2e0df681f18af2233598d62f3669208
unknown <alinanumanova555@gmail.com>
2026-02-18 15:38:32 +0300

    add: navigation sidebar

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ categoryChips.forEach((chip) => {
 // category switch buttons
 const categorySwitchButtons = document.querySelectorAll(".card");
 categorySwitchButtons.forEach((menuCategoryButton) => {
-    menuCategoryButton.addEventListener("click", () => {
-        categorySwitchButtons.forEach((el) => {
-            el.classList.remove("active-menu-category");
-        });
-        menuCategoryButton.classList.add("active-menu-category");
+  menuCategoryButton.addEventListener("click", () => {
+    categorySwitchButtons.forEach((el) => {
+      el.classList.remove("active-menu-category");
     });
+    menuCategoryButton.classList.add("active-menu-category");
+  });
 });
 //navigation side-bar
-const navBar = document.querySelector('.nav-bar');
-navBar.addEventListener('click', () => {
-  navBar.classList.add('open');
+const navBar = document.querySelector(".nav-bar");
+navBar.addEventListener("click", () => {
+  navBar.classList.add("open");
 });
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
   if (!navBar.contains(event.target)) {
-    navBar.classList.remove('open');
+    navBar.classList.remove("open");
   }
 });
+
+const nav = document.querySelector(".nav-bar");
+const body = document.body;
+
+function toggleMenu() {
+  nav.classList.toggle("open");
+
+  if (nav.classList.contains("open")) {
+    body.style.marginLeft = "120px";
+  } else {
+    body.style.marginLeft = "0px";
+  }
+}
